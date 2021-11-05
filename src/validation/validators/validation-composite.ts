31256bdb08bdf9d69ac6cfc3e3b33ba8f7191c38
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 17:31:12 -0300

    feat: add validationComposite

---
+++ src/validation/validators/validation-composite.ts
@@ -0,0 +1,17 @@
+import { Validation } from "../../presentation/controllers/protocols/validation"
+/**
+ * this class is a composite of validators
+ * is a validator that receive an array of validators and then performs each validation
+ */
+export class ValidationComposite implements Validation {
+  constructor (private readonly validations: Validation[]) { }
+
+  validate (input: any): Error {
+    for (const validation of this.validations) {
+      const error = validation.validate(input)
+      if (error) {
+        return error
+      }
+    }
+  }
+}
