6c2f27e6b4e44b05baf7c35206e11f74d2d475fc
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 11:00:01 -0300

    feat: add methods getCollection and map to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,5 @@
 
-import { MongoClient, MongoClientOptions } from 'mongodb'
+import { Collection, MongoClient, MongoClientOptions } from 'mongodb'
 export const MongoHelper = {
   client: null as MongoClient,
   uri: null as string,
@@ -10,5 +10,15 @@ export const MongoHelper = {
   async disconnect (): Promise<void> {
     await this.client.close()
     this.client = null
+  },
+    async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected) {
+      await this.connect(this.uri)
+    }
+    return this.client.db().collection(name)
+  },
+  map: (collection: any): any => {
+    const { _id, ...collectionWOId } = collection
+    return Object.assign({}, collectionWOId, { id: _id })
   }
 }
