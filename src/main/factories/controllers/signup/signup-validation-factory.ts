9ac08f1513df9a69e55d60c61bb47c5a499398f4
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 17:40:30 -0300

    feat: create a signup validation factory to avoid lost factories on the code

---
+++ src/main/factories/controllers/signup/signup-validation-factory.ts
@@ -0,0 +1,15 @@
+import { Validation } from "../../../../presentation/controllers/protocols/validation"
+import { CompareFieldsValidaiton } from "../../../../validation/validators/compare-fields-validation"
+import { EmailValidation } from "../../../../validation/validators/email-validation"
+import { RequiredFieldValidation } from "../../../../validation/validators/required-field-validation"
+import { ValidationComposite } from "../../../../validation/validators/validation-composite"
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirm']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new CompareFieldsValidaiton('password', 'passwordConfirm'))
+  validations.push(new EmailValidation('email'))
+  return new ValidationComposite(validations)
+}
