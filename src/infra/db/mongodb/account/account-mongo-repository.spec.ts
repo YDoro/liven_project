5c6ca4643fed6d4bcbf7cb7f47a0aef05e4f6e07
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 11:12:57 -0300

    test: ensure AccountMongoRepository will return an account on add

---
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -0,0 +1,37 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account-mongo-repository'
+
+let accountCollection: Collection
+
+describe('account mongo repository', () => {
+    beforeAll(async () => {
+      await MongoHelper.connect(process.env.MONGO_URL)
+    })
+    afterAll(async () => {
+      await MongoHelper.disconnect()
+    })
+    beforeEach(async () => {
+      accountCollection = await MongoHelper.getCollection('accounts')
+      await accountCollection.deleteMany({})
+    })
+
+    const makeSut = (): AccountMongoRepository => {
+      return new AccountMongoRepository()
+    }
+
+    test('Should return an account on add success', async () => {
+      const sut = makeSut()
+      const account = await sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+
+})
