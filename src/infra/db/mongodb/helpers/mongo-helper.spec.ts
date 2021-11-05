e406de5e67837ede2ad58790c82c6935cba4490b
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 11:00:35 -0300

    test: ensure MongoHelper will get collection even if connection is down

--- src/infra/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -10,4 +10,18 @@ describe('Mongo Helper', () => {
     expect(sut.client).toBeNull()
 
   })
+  test('should get collection', async () => {
+    await sut.connect(process.env.MONGO_URL)
+    const accountCollection = await sut.getCollection('accounts')
+
+    expect(accountCollection).toBeTruthy()
+  })
+  
+  test('Should reconnect if mongodb is down', async () => {
+    let accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
 })
