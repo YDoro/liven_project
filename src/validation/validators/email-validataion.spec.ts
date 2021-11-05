10c174f0624ba297d5b75ce311e8a465ec548a47
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 17:13:41 -0300

    test: ensure email validator will return null on fail and error on success

---
+++ src/validation/validators/email-validataion.spec.ts
@@ -0,0 +1,28 @@
+import { InvalidParamError } from '../../presentation/errors/http/invalid-param-error'
+import { EmailValidation } from './email-validation'
+
+interface SutTypes{
+  sut: EmailValidation
+}
+
+
+const makeSut = (): SutTypes => {
+  const sut = new EmailValidation('email')
+  return {
+    sut
+  }
+}
+
+describe('email validation', () => {
+  test('should return an error if email is invalid', () => {
+    const { sut} = makeSut()
+    const error = sut.validate({ email: 'invalid_emailmail.com' })
+    expect(error).toEqual(new InvalidParamError('email'))
+  })
+  
+  test('should return null if email is valid', () => {
+    const { sut} = makeSut()
+    const error = sut.validate({ email: 'valid_email@mail.com' })
+    expect(error).toBeNull()
+  })
+})
