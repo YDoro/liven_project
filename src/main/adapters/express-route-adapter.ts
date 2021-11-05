bc0bb3f4fe223024b7bca35228ae677bace334de
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 17:59:13 -0300

    feat: add expressRouteAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+import { Controller } from '../../presentation/controllers/protocols/controller'
+import { HttpRequest } from '../../presentation/controllers/protocols/http'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    httpResponse.body.message
+      ? res.status(httpResponse.statusCode).json({ 
+        error: httpResponse.body.message
+       })
+      : res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
