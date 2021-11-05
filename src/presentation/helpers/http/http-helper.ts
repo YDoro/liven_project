71e80a623dd5daf6d7778988efd8fb21819ff402
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 17:00:13 -0300

    feat: add http created helper

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,12 +1,17 @@
 import { HttpResponse } from '../../controllers/protocols/http'
 import { ServerError } from '../../errors/http/server-error'
 
-export const serverError = (error: Error): HttpResponse => ({
-  statusCode: 500,
-  body: new ServerError(error.stack)
+export const created = (data:any): HttpResponse => ({
+  statusCode: 201,
+  body: data
 })
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
 })
