714cfb8457266c9f08dbfc627625c3758f7a219d
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 18:33:15 -0300

    feat: implements loadAccountByEmail on AccountMongoRepository

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,9 +1,11 @@
 import { AddAccountRepository } from "../../../../data/protocols/db/account/add-account-repository";
+import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/account/load-account-by-email-repository";
 import { AccountModel } from "../../../../domain/models/account";
 import { AddAccountModel } from "../../../../domain/usecases/add-account";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class AccountMongoRepository implements AddAccountRepository{
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository{
+
     private accountCollection = 'accounts'
 
     async add(accountData: AddAccountModel): Promise<AccountModel> {
@@ -11,5 +13,11 @@ export class AccountMongoRepository implements AddAccountRepository{
         const result = await accountCollection.findOne({_id:(await accountCollection.insertOne(accountData)).insertedId})
         return MongoHelper.map(result)
     }
+
+    async loadByEmail (email: string): Promise<AccountModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        const account = await accountCollection.findOne({ email })
+        return account && MongoHelper.map(account)
+      }
 
 }
