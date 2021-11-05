d94f114f0d5de25af37ab8b8adb59552d2fcb495
Yan Mello <yan_mvd@hotmail.com>
2021-11-05 16:20:57 -0300

    feat: add badRequest http helper

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,7 +1,12 @@
-import { HttpResponse } from "../../controllers/protocols/http";
-import { ServerError } from "../../errors/http/server-error";
+import { HttpResponse } from '../../controllers/protocols/http'
+import { ServerError } from '../../errors/http/server-error'
 
 export const serverError = (error: Error): HttpResponse => ({
-    statusCode: 500,
-    body: new ServerError(error.stack)
-  })
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
