35e8503a4c3dff78bf6cc875d00f2ecfbffb623f
Yan Mello <yan_mvd@hotmail.com>
2021-11-04 23:14:22 -0300

    fix: using dotenv file if it exists

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,9 @@
 import {MongoHelper} from'../infra/db/mongodb/helpers/mongo-helper'
+require('dotenv').config() //for non docker runnings use a .env file
 import env from './config/env'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
-    const app = require('./config/app')
+    const app = require('./config/app').default
     app.listen(env.port, () => console.log(`Server running on port http://localhost:${env.port}`))
   }).catch(console.error)
