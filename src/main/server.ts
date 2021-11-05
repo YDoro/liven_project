eca9a31f7825f5c8e1758d64312c02e8df316fb0
Yan Mello <yan_mvd@hotmail.com>
2021-11-04 23:04:30 -0300

    refactor: move app instance to another file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,8 @@
-import express from 'express'
 import {MongoHelper} from'../infra/db/mongodb/helpers/mongo-helper'
 import env from './config/env'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
-    const app = express()
+    const app = require('./config/app')
     app.listen(env.port, () => console.log(`Server running on port http://localhost:${env.port}`))
   }).catch(console.error)
