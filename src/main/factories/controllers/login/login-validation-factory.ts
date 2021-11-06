1feb6c08c3cc3665c381c598c919fa10799eb28d
Yan Mello <yan_mvd@hotmail.com>
2021-11-06 11:33:10 -0300

    feat: add login validation factory

---
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -0,0 +1,14 @@
+import { Validation } from "../../../../presentation/controllers/protocols/validation"
+import { EmailValidation } from "../../../../validation/validators/email-validation"
+import { RequiredFieldValidation } from "../../../../validation/validators/required-field-validation"
+import { ValidationComposite } from "../../../../validation/validators/validation-composite"
+
+export const makeLoginValidation = (): ValidationComposite => {
+    const validations: Validation[] = []
+    for (const field of ['email', 'password']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(new EmailValidation('email'))
+    return new ValidationComposite(validations)
+  }
+  
