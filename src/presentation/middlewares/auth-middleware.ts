69a2dafe1d905f15dcacad404b5ebfc72922cf56
Yan Mello <yan_mvd@hotmail.com>
2021-11-06 15:21:38 -0300

    feat: ensure AuthMiddleware will return 200 if user returned from loadAccountByToken

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -2,14 +2,15 @@ import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token"
 import { HttpRequest, HttpResponse } from "../controllers/protocols/http";
 import { Middleware } from "../controllers/protocols/middleware";
 import { AccessDeniedError } from "../errors/access-denied-error";
-import { forbidden } from "../helpers/http/http-helper";
+import { forbidden, ok } from "../helpers/http/http-helper";
 
 export class AuthMiddleware implements Middleware{
     constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const accessToken = httpRequest.headers?.['x-access-token']
         if(accessToken){
-            await this.loadAccountByToken.load(accessToken)
+            const user = await this.loadAccountByToken.load(accessToken)
+            if(user) return ok(user)
         }
         
         return forbidden(new AccessDeniedError())
