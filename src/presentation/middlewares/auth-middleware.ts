4b56547f97694dc454106bc4daacafcc36277547
Yan Mello <yan_mvd@hotmail.com>
2021-11-06 13:31:01 -0300

    feat: add LoadAccountByToken usecase to AuthMiddleware

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,11 +1,17 @@
+import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
 import { HttpRequest, HttpResponse } from "../controllers/protocols/http";
 import { Middleware } from "../controllers/protocols/middleware";
 import { AccessDeniedError } from "../errors/access-denied-error";
 import { forbidden } from "../helpers/http/http-helper";
 
 export class AuthMiddleware implements Middleware{
+    constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const error =new AccessDeniedError()
-        return new Promise(resolve=>resolve(forbidden(error)))
+        const accessToken = httpRequest.headers?.['x-access-token']
+        if(accessToken){
+            await this.loadAccountByToken.load(accessToken)
+        }
+        
+        return forbidden(new AccessDeniedError())
     }
 }
