431a908cbfaccc576192e695429c5f386f026fba
Yan Mello <yan_mvd@hotmail.com>
2021-11-06 11:16:53 -0300

    feat: add unauthorized helper

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,5 +1,6 @@
 import { HttpResponse } from '../../controllers/protocols/http'
 import { ServerError } from '../../errors/http/server-error'
+import { UnauthorizedError } from '../../errors/http/unauthorized-error'
 
 export const created = (data:any): HttpResponse => ({
   statusCode: 201,
@@ -9,6 +10,10 @@ export const created = (data:any): HttpResponse => ({
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
+})
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
 })
 
 export const serverError = (error: Error): HttpResponse => ({
