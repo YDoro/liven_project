2158ff40252c11af85c107cd18be28d9353ccbe4
Yan Mello <yan_mvd@hotmail.com>
2021-11-06 12:31:40 -0300

    test: ensure signupController will return 403 on email in use and throw if authentication throws

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -1,5 +1,8 @@
 import { AccountModel } from '../../domain/models/account'
 import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
+import { Authentication, AuthenticationModel } from '../../domain/usecases/authentication'
+import { EmailAlreadyInUseError } from '../errors/email-already-in-use-error'
+import { forbidden, serverError } from '../helpers/http/http-helper'
 import { HttpRequest } from './protocols/http'
 import { Validation } from './protocols/validation'
 import { SignUpController } from './signup-controller'
@@ -8,6 +11,7 @@ interface SutTypes {
   sut: SignUpController
   addAccountStub: AddAccount
   validationStub: Validation
+  authenticatioStub: Authentication
 }
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -43,12 +47,21 @@ const makeValidation = (): Validation => {
   }
   return new ValidationStub()
 }
+const makeAuthentication = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth (authentication: AuthenticationModel): Promise<string> {
+      return await new Promise(resolve => resolve('any_token'))
+    }
+  }
+  return new AuthenticationStub()
+}
 
 const makeSut = ():SutTypes => {
   const validationStub = makeValidation()
   const addAccountStub = makeAddAccount()
-  const sut = new SignUpController(addAccountStub, validationStub)
-  return { addAccountStub, sut, validationStub }
+  const authenticatioStub = makeAuthentication()
+  const sut = new SignUpController(addAccountStub, validationStub,authenticatioStub)
+  return { addAccountStub, sut, validationStub ,authenticatioStub}
 }
 
 describe('signup controller', () => {
@@ -77,6 +90,13 @@ describe('signup controller', () => {
      expect(response.statusCode).toBe(400)
   })
 
+  test('should return 403 if add account returns null', async () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockResolvedValueOnce(null)
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbidden(new EmailAlreadyInUseError()))
+  })
+  
   test('should return 500 if addAccount throws', async () => {
     const { sut, addAccountStub } = makeSut()
      jest.spyOn(addAccountStub, 'add').mockRejectedValueOnce(new Error('any error'))
@@ -89,6 +109,20 @@ describe('signup controller', () => {
      jest.spyOn(validationStub, 'validate').mockImplementationOnce(()=>{throw new Error('any error')})
      const response = await sut.handle(makeFakeRequest())
      expect(response.statusCode).toBe(500)
+  })
+
+  test('Should call Authentication with correct values', async () => {
+    const { sut, authenticatioStub } = makeSut()
+    const authSpy = jest.spyOn(authenticatioStub, 'auth')
+    await sut.handle(makeFakeRequest())
+    expect(authSpy).toHaveBeenCalledWith({ email: makeFakeRequest().body.email, password: makeFakeRequest().body.password })
+  })
+
+  test('Should should return 500 if Authentication throws', async () => {
+    const { sut, authenticatioStub } = makeSut()
+    jest.spyOn(authenticatioStub, 'auth').mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 
 })
