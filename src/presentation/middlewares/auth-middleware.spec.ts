f0706e9633e15d17dddeb9ce616fa08eaa9c65fc
Yan Mello <yan_mvd@hotmail.com>
2021-11-06 13:31:44 -0300

    test: ensure AuthMiddleware will call loadAccountByToken with token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,49 @@
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import {AuthMiddleware} from './auth-middleware'
+import { LoadAccountByToken} from '../../domain/usecases/load-account-by-token'
+import { AccountModel } from '../../domain/models/account'
+
+interface SutTypes{
+    loadAccountByTokenStub: LoadAccountByToken
+    sut:AuthMiddleware
+}
+
+const makeLoadAccountbyToken = ():LoadAccountByToken=>{
+    class LoadAccountByTokenStub implements LoadAccountByToken{
+        async load(accessToken: string): Promise<AccountModel> {
+            return new Promise(resolve=>resolve(makeFakeAccount()))
+        }
+    }
+    return new LoadAccountByTokenStub()
+}
+
+const makeSUT = ():SutTypes=>{
+    const loadAccountByTokenStub = makeLoadAccountbyToken()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    return {sut,loadAccountByTokenStub}
+}
+const makeFakeAccount = ():AccountModel =>({
+    id:'valid_id',
+    name:'valid_name',
+    email:'valid_email@mail.com',
+    password:'hashed_password'
+})
 
 describe('auth middleware',()=>{
     test('should return 403 if no authentication is provided',async ()=>{
-        const sut = new AuthMiddleware()
+        const {sut} = makeSUT() 
         const httpResponse = await sut.handle({})
         expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+    })
+
+    test('should call loadAccountByToken with correct accessToken',async ()=>{
+        const {loadAccountByTokenStub,sut} = makeSUT()
+        const loadSpy = jest.spyOn(loadAccountByTokenStub,'load')
+
+        await sut.handle({
+            headers:{'x-access-token':'any_token'}
+        })
+        expect(loadSpy).toHaveBeenCalledWith('any_token')
     })
 })
