b5b56df41f22e527d608a4cb0ef1cdc116b9b7f3
Yan Mello <yan_mvd@hotmail.com>
2021-11-07 19:17:37 -0300

    test: ensure unique address name validation will return an error if address name already in use

---
+++ src/validation/validators/unique-address-name-validation.spec.ts
@@ -0,0 +1,41 @@
+import { ListAddressesRepository } from '../../data/protocols/db/address/list-addresses-repository'
+import { AddressModel } from '../../domain/models/address'
+import { UniqueParamError } from '../../presentation/errors/unique-param-error'
+import { UniqueAddressNameValidation } from './unique-address-name-validation'
+interface SutTypes{
+    sut:UniqueAddressNameValidation,
+    listAddressesRepositoryStub:ListAddressesRepository
+}
+const makeFakeAddress = ():AddressModel => ({
+  name: 'any_name',
+  street: 'any_street',
+  number: 'any_number',
+  complement: 'any_complement',
+  landmark: 'any_landmark',
+  city: 'any_city',
+  state: 'any_state',
+  country: 'any_country',
+  neigborhood: 'any_neighborhood',
+  postalcode: '12345678'
+})
+
+const makeListAddressRepository = ():ListAddressesRepository => {
+  class ListAddressesRepositoryStub implements ListAddressesRepository {
+    async list (accountId: string): Promise<AddressModel[]> {
+      return new Promise(resolve => resolve([makeFakeAddress()]))
+    }
+  }
+  return new ListAddressesRepositoryStub()
+}
+const makeSUT = ():SutTypes => {
+  const listAddressesRepositoryStub = makeListAddressRepository()
+  const sut = new UniqueAddressNameValidation('any_account_id', listAddressesRepositoryStub)
+  return { listAddressesRepositoryStub, sut }
+}
+describe('unique address name validation ', () => {
+  test('should return an uniqueParamError if address found values', async () => {
+    const { sut } = makeSUT()
+    const error = await sut.validate({ name: 'any_name' })
+    expect(error).toBeInstanceOf(UniqueParamError)
+  })
+})
