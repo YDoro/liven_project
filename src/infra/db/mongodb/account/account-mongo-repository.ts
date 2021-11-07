de4c34dcc73fdae4c9e14b8e8fbdcafb96c9a341
Yan Mello <yan_mvd@hotmail.com>
2021-11-07 11:36:44 -0300

    feat: ensure AccountMongoRepository loadByToken  will return an account on success

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,11 +1,18 @@
+import { resolve } from "path/posix";
 import { AddAccountRepository } from "../../../../data/protocols/db/account/add-account-repository";
 import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/account/load-account-by-email-repository";
+import { LoadAccountByTokenRepository } from "../../../../data/protocols/db/account/load-account-by-token-repository";
 import { UpdateAccessTokenRepository } from "../../../../data/protocols/db/account/update-access-token-repository";
 import { AccountModel } from "../../../../domain/models/account";
 import { AddAccountModel } from "../../../../domain/usecases/add-account";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository{
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository{
+  async loadByToken(token: string): Promise<AccountModel> {
+      const accountCollection = await MongoHelper.getCollection(this.accountCollection)
+      const account = await accountCollection.findOne({ accessToken:token })
+      return account && MongoHelper.map(account)
+    }
 
     private accountCollection = 'accounts'
 
