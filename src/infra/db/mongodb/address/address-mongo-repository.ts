e6e5a24de49e119f8193d07887578ca905b82b83
Yan Mello <yan_mvd@hotmail.com>
2021-11-07 18:43:56 -0300

    feat: now DbAddressMongoReposity implemens ListAddresses usecase

--- src/infra/db/mongodb/address/address-mongo-repository.ts
+++ src/infra/db/mongodb/address/address-mongo-repository.ts
@@ -1,11 +1,18 @@
 import { MongoTransactionError, PushOperator, ObjectId } from 'mongodb'
 import { AddAddressRepository } from '../../../../data/protocols/db/address/add-address-repository'
+import { ListAddressesRepository } from '../../../../data/protocols/db/address/list-addresses-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddressModel } from '../../../../domain/models/address'
 import { AccountMongoRepository } from '../account/account-mongo-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AddressMongoRepository implements AddAddressRepository {
+export class AddressMongoRepository implements AddAddressRepository, ListAddressesRepository {
+  async list (accountId: string): Promise<AddressModel[]> {
+    const accountCollection = await MongoHelper.getCollection(AccountMongoRepository.accountCollection)
+    const account = await accountCollection.findOne<AccountModel>({ _id: new ObjectId(accountId) })
+    return account.addresses
+  }
+
   async add (address: AddressModel, userId: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection(AccountMongoRepository.accountCollection)
     const result = await accountCollection.findOneAndUpdate(
