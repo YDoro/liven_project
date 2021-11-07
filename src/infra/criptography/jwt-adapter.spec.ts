fca0edd8f2eed8dab06452ade75900444048aa9f
Yan Mello <yan_mvd@hotmail.com>
2021-11-07 11:18:24 -0300

    test: ensure JWTAdapter will return the encrypted value

--- src/infra/criptography/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter.spec.ts
@@ -1,4 +1,3 @@
-import { verify } from "crypto";
 import jwt from "jsonwebtoken";
 import { JwtAdapter } from "./jwt-adapter";
 jest.mock("jsonwebtoken", () => ({
@@ -43,6 +42,12 @@ describe("jwt adapter", () => {
       const signSpy = jest.spyOn(jwt, "verify");
       await sut.decrypt("any_token");
       expect(signSpy).toHaveBeenCalledWith("any_token", "secret");
+    });
+
+    test("should return a valuye on verify success", async () => {
+      const sut = makeSut();
+      const value = await sut.decrypt("any_token");
+      expect(value).toBe("any_value");
     });
   })
 });
