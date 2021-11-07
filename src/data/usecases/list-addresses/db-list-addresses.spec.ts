b67bb14791728bec4f996c8984b3e46e641f7fdd
Yan Mello <yan_mvd@hotmail.com>
2021-11-07 18:30:58 -0300

    test: ensure DbListAddresses wil call listAddressRepository with right params

---
+++ src/data/usecases/list-addresses/db-list-addresses.spec.ts
@@ -0,0 +1,43 @@
+import { AddressModel } from '../../../domain/models/address'
+import { ListAddressesRepository } from '../../protocols/db/address/list-addresses-repository'
+import { DbListAddresses } from './db-list-addresses'
+interface SutTypes{
+    sut: DbListAddresses,
+    listAddressesRepositoryStub: ListAddressesRepository
+}
+
+const makeFakeAddress = ():AddressModel => ({
+  name: 'any_name',
+  street: 'any_street',
+  number: 'any_number',
+  complement: 'any_complement',
+  landmark: 'any_landmark',
+  city: 'any_city',
+  state: 'any_state',
+  country: 'any_country',
+  neigborhood: 'any_neighborhood',
+  postalcode: '12345678'
+})
+
+const makeListAddresRepository = ():ListAddressesRepository => {
+  class ListAddressesRepositoryStub implements ListAddressesRepository {
+    list (accountId: string): Promise<AddressModel[]> {
+      return new Promise(resolve => resolve([makeFakeAddress()]))
+    }
+  }
+  return new ListAddressesRepositoryStub()
+}
+
+const makeSUT = ():SutTypes => {
+  const listAddressesRepositoryStub = makeListAddresRepository()
+  const sut = new DbListAddresses(listAddressesRepositoryStub)
+  return { listAddressesRepositoryStub, sut }
+}
+describe('db list addresses', () => {
+  test('should call ListAddressesRepository with the right values', async () => {
+    const { sut, listAddressesRepositoryStub } = makeSUT()
+    const listSpy = jest.spyOn(listAddressesRepositoryStub, 'list')
+    await sut.list('any_account_id')
+    expect(listSpy).toHaveBeenCalledWith('any_account_id')
+  })
+})
