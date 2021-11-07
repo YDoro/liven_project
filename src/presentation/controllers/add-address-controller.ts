5df2845f55e224bf7f269af936e416ae2936a512
Yan Mello <yan_mvd@hotmail.com>
2021-11-07 16:43:00 -0300

    feat: ensure addAddressController will return 400 on validation Error

---
+++ src/presentation/controllers/add-address-controller.ts
@@ -0,0 +1,20 @@
+import { AddAddress } from '../../domain/usecases/add-address'
+import { badRequest } from '../helpers/http/http-helper'
+import { Controller } from './protocols/controller'
+import { HttpRequest, HttpResponse } from './protocols/http'
+import { Validation } from './protocols/validation'
+
+export class AddAddressController implements Controller {
+  constructor (
+        private readonly addAddress: AddAddress,
+        private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
+    return new Promise(resolve => resolve({ statusCode: 200, body: {} }))
+  }
+}
