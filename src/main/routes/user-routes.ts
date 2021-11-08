e96e877717f3285b28f0b2633ac4ae5b174c70f0
Yan Mello <yan_mvd@hotmail.com>
2021-11-08 20:10:31 -0300

    feat: ensure user data will be updated on patch - return status should be 200|304|403|500

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { GetUserController } from '../factories/controllers/get-user/get-user-controller'
+import { makeUpdateAccountController } from '../factories/controllers/update-account/update-account-controller-factory'
 import { makeAuthMiddleware } from '../factories/middleware/auth-middleware-factory'
 
 export default (router: Router): void => {
   router.get('/user', adaptMiddleware(makeAuthMiddleware(true)), adaptRoute(new GetUserController()))
+  router.patch('/user', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeUpdateAccountController()))
 }
