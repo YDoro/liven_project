bf549e571c9e6b443b3beceda0a268d84ef1a7d6
Yan Mello <yan_mvd@hotmail.com>
2021-11-08 19:36:25 -0300

    feat: ensure not required fields will be validated

---
+++ src/validation/validators/optional-field-composite-validation.ts
@@ -0,0 +1,22 @@
+import { Validation } from '../../presentation/controllers/protocols/validation'
+export interface MapFieldValidation{
+    field:string,
+    validation:Validation
+}
+export class OptionalFieldValidationComposite implements Validation {
+  constructor (private readonly validations: MapFieldValidation[]) { }
+
+  async validate (input: any): Promise<Error> {
+    for (const validationMap of this.validations) {
+      if (input[validationMap.field]) {
+        const error = validationMap.validation.validate(input)
+        if (error) {
+          if (error instanceof Promise) {
+            return await error
+          }
+          return error
+        }
+      }
+    }
+  }
+}
