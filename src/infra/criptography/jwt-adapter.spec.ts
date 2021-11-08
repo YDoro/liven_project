ac826db5709bea6be92d8e65afdf956d89812919
Yan Mello <yan_mvd@hotmail.com>
2021-11-08 20:16:14 -0300

    fix: avoid server errors on fail to decode token

--- src/infra/criptography/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter.spec.ts
@@ -50,13 +50,13 @@ describe('jwt adapter', () => {
       expect(value).toBe('any_value')
     })
 
-    test('should throw if verify throws', async () => {
+    test('should not throw if verify throws', async () => {
       const sut = makeSut()
       jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
         throw new Error()
       })
       const promise = sut.decrypt('any_token')
-      await expect(promise).rejects.toThrow()
+      await expect(promise).resolves.not.toThrow()
     })
   })
 })
