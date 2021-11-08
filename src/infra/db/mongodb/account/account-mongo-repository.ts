b067b706d513824e3a0e4f7e41390f8e4ef34bd0
Yan Mello <yan_mvd@hotmail.com>
2021-11-08 18:28:32 -0300

    feat: ensure AccountMongoRepository will return true or false on update account

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -3,11 +3,13 @@ import { AddAccountRepository } from '../../../../data/protocols/db/account/add-
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { LoadAccountByTokenRepository } from '../../../../data/protocols/db/account/load-account-by-token-repository'
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository'
+import { UpdateAccountRepository } from '../../../../data/protocols/db/account/update-account-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { UpdateAccountModel } from '../../../../domain/usecases/update-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByTokenRepository, UpdateAccountRepository {
   async loadByToken (token: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection(AccountMongoRepository.accountCollection)
     const account = await accountCollection.findOne({ accessToken: token })
@@ -34,5 +36,14 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
       { _id: new ObjectId(id) }, {
         $set: { accessToken: token }
       })
+  }
+
+  async update (data: UpdateAccountModel, id: string): Promise<boolean> {
+    const accountCollection = await MongoHelper.getCollection(AccountMongoRepository.accountCollection)
+    const updated = await accountCollection.updateOne(
+      { _id: new ObjectId(id) }, {
+        $set: data
+      })
+    return updated.modifiedCount > 0
   }
 }
