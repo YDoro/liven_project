13fe165c33acd4691ef2d5773d9bb281d00fd613
Yan Mello <yan_mvd@hotmail.com>
2021-11-08 16:50:07 -0300

    feat: ensure getUserController will return 200 on success or 500 on fail

---
+++ src/main/factories/controllers/get-user/get-user-controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '../../../../presentation/controllers/protocols/controller'
+import { HttpRequest, HttpResponse } from '../../../../presentation/controllers/protocols/http'
+import { ok, serverError } from '../../../../presentation/helpers/http/http-helper'
+
+export class GetUserController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (httpRequest.body.middleware.user) {
+      return ok(httpRequest.body.middleware.user)
+    }
+    return serverError(new Error('user not provided by middleware'))
+  }
+}
