b49fe3cf9a1e61c6d3553c42b8d464ef2f7808da
Yan Mello <yan_mvd@hotmail.com>
2021-11-08 09:18:58 -0300

    feat: ensure numeric field validation will return InvalidParamError on oversized field given

--- src/validation/validators/numeric-field-validation.ts
+++ src/validation/validators/numeric-field-validation.ts
@@ -2,11 +2,14 @@ import { Validation } from '../../presentation/controllers/protocols/validation'
 import { InvalidParamError } from '../../presentation/errors/invalid-param-error'
 
 export class NumericFieldValidation implements Validation {
-  constructor (private readonly fieldName: string, private readonly size?:string) {}
+  constructor (private readonly fieldName: string, private readonly maxSize?:number) {}
 
   validate (input: any): Error | Promise<Error> {
     const asNum = Number(input[this.fieldName])
     if (isNaN(asNum)) {
+      return new InvalidParamError(this.fieldName)
+    }
+    if (this.maxSize && asNum.toString().length > this.maxSize) {
       return new InvalidParamError(this.fieldName)
     }
   }
