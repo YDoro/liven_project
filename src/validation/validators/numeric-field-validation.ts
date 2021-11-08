27b6f3fece3544810e5912bcb9ee73b24b1b09ad
Yan Mello <yan_mvd@hotmail.com>
2021-11-08 09:22:47 -0300

    feat: ensure numeric field validation will return InvalidParamError on undersized field given

--- src/validation/validators/numeric-field-validation.ts
+++ src/validation/validators/numeric-field-validation.ts
@@ -2,11 +2,14 @@ import { Validation } from '../../presentation/controllers/protocols/validation'
 import { InvalidParamError } from '../../presentation/errors/invalid-param-error'
 
 export class NumericFieldValidation implements Validation {
-  constructor (private readonly fieldName: string, private readonly maxSize?:number) {}
+  constructor (private readonly fieldName: string, private readonly maxSize?:number, private readonly minSize?:number) {}
 
   validate (input: any): Error | Promise<Error> {
     const asNum = Number(input[this.fieldName])
     if (isNaN(asNum)) {
+      return new InvalidParamError(this.fieldName)
+    }
+    if (this.minSize && asNum.toString().length < this.minSize) {
       return new InvalidParamError(this.fieldName)
     }
     if (this.maxSize && asNum.toString().length > this.maxSize) {
