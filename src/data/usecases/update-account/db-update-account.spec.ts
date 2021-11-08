8e46323ff2ceee61b37269beca60df486f998378
Yan Mello <yan_mvd@hotmail.com>
2021-11-08 18:10:28 -0300

    feat: ensure hasher will only be called if password is given

--- src/data/usecases/update-account/db-update-account.spec.ts
+++ src/data/usecases/update-account/db-update-account.spec.ts
@@ -37,8 +37,8 @@ describe('update account db usecase', () => {
   test('should call UpdateAccountRepository update with the right values', async () => {
     const { sut, updateAccountRepositoryStub } = makeSUT()
     const updateSpy = jest.spyOn(updateAccountRepositoryStub, 'update')
-    await sut.update({ password: 'new_password' }, 'any_id')
-    expect(updateSpy).toHaveBeenCalledWith({ password: 'new_password' }, 'any_id')
+    await sut.update({ name: 'new_name' }, 'any_id')
+    expect(updateSpy).toHaveBeenCalledWith({ name: 'new_name' }, 'any_id')
   })
   test('should return true if UpdateAccountRepository update returns true', async () => {
     const { sut } = makeSUT()
@@ -51,9 +51,37 @@ describe('update account db usecase', () => {
     const wasUpdated = await sut.update({ password: 'new_password' }, 'any_id')
     expect(wasUpdated).toBe(false)
   })
-  test('should return throw if UpdateAccountRepository update throws', async () => {
+  test('should throw if UpdateAccountRepository update throws', async () => {
     const { sut, updateAccountRepositoryStub } = makeSUT()
     jest.spyOn(updateAccountRepositoryStub, 'update').mockRejectedValueOnce(new Error('any_error'))
+    const promise = sut.update({ password: 'new_password' }, 'any_id')
+    expect(promise).rejects.toThrowError('any_error')
+  })
+  test('should call Hasher hash with the right values if password given', async () => {
+    const { sut, hasherStub } = makeSUT()
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+    await sut.update({ password: 'new_password' }, 'any_id')
+    expect(hashSpy).toHaveBeenCalledWith('new_password')
+  })
+
+  test('should not call Hasher hash with if password not given', async () => {
+    const { sut, hasherStub } = makeSUT()
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+    await sut.update({ name: 'new_name' }, 'any_id')
+    expect(hashSpy).not.toHaveBeenCalledWith('new_password')
+  })
+
+  test('should call Hasher and then call updateAccessTokenRepository if password given', async () => {
+    const { sut, hasherStub, updateAccountRepositoryStub } = makeSUT()
+    const updateSpy = jest.spyOn(updateAccountRepositoryStub, 'update')
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+    await sut.update({ password: 'new_password' }, 'any_id')
+    expect(hashSpy).toHaveBeenCalledWith('new_password')
+    expect(updateSpy).toHaveBeenCalledWith({ password: 'hashed_password' }, 'any_id')
+  })
+  test('should throw if hahser hash throws', async () => {
+    const { sut, hasherStub } = makeSUT()
+    jest.spyOn(hasherStub, 'hash').mockRejectedValueOnce(new Error('any_error'))
     const promise = sut.update({ password: 'new_password' }, 'any_id')
     expect(promise).rejects.toThrowError('any_error')
   })
