bcf3c443b384339a9949c3f109175ea18e5b23a0
Yan Mello <yan_mvd@hotmail.com>
2021-11-12 15:44:07 -0300

    feat: remove user password from response

--- src/main/factories/controllers/get-user/get-user-controller.ts
+++ src/main/factories/controllers/get-user/get-user-controller.ts
@@ -5,6 +5,7 @@ import { ok, serverError } from '../../../../presentation/helpers/http/http-help
 export class GetUserController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (httpRequest.body.middleware.user) {
+      delete httpRequest.body.middleware.user.password
       return ok(httpRequest.body.middleware.user)
     }
     return serverError(new Error('user not provided by middleware'))
