d86e61ff32d4d29e791253f84dfa07a8888a1ebd
Yan Mello <yan_mvd@hotmail.com>
2021-11-12 10:15:12 -0300

    refactor: remove unecessary ifs

--- src/presentation/helpers/object/flatten-object.ts
+++ src/presentation/helpers/object/flatten-object.ts
@@ -2,12 +2,9 @@ export const flattenObject = (obj:any):any => {
   const toReturn = {}
 
   for (const i in obj) {
-    if (!Object.prototype.hasOwnProperty.call(obj, i)) continue
-
     if ((typeof obj[i]) === 'object' && obj[i] !== null) {
       const flatObject = flattenObject(obj[i])
       for (const x in flatObject) {
-        if (!Object.prototype.hasOwnProperty.call(flatObject, x)) continue
         toReturn[i + '.' + x] = flatObject[x]
       }
     } else {
